2186b8025d0d5a819bbf297bc2124508ed2aa0f1
Mehtaab Gill <mehtaabgill@hotmail.com>
2021-05-08 11:55:52 -0600

    feat(app): started ImagesDisplay component

---
+++ src/components/ImagesDisplay.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="grid grid-cols-3 gap-4 justify-items-center"></div>
+</template>
+
+<script>
+export default {
+  name: "ImagesDisplay",
+};
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <ImagesDisplay class="mx-8 my-8"></ImagesDisplay>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import ImagesDisplay from "@/components/ImagesDisplay";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    ImagesDisplay,
+  },
+});
+</script>
